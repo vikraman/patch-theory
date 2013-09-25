f7786fdcb48ce11faaa8b3e1f51e81c4435ffc58
Vikraman <vikraman@cse.iitk.ac.in>
2013-09-25 11:50:07 +0530

    Use the list in the patch also

--- src/Data/Patch/Instances/List.hs
+++ src/Data/Patch/Instances/List.hs
@@ -10,14 +10,14 @@ import Data.Patch.Commute
 import Data.Patch.Invert
 
 data Primitive a from to where
-  Change :: Eq a => Int -> a -> a -> Primitive a from to
+  Change :: Eq a => [a] -> Int -> a -> a -> Primitive a from to
 
 instance Invert (Primitive a) where
-  invert (Change i a b) = Change i b a
+  invert (Change l i a b) = Change l i b a
 
 instance Commute (Primitive a) (Primitive a) where
   commute (p `Then` q) = case (p, q) of
-    (Change i x y, Change j z w) | i == j && y /= z -> Nothing
-                                 | otherwise -> Just (q' `Then` p')
+    (Change l i x y, Change l' j z w) | l == l' && i == j && y /= z -> Nothing
+                                      | otherwise -> Just (q' `Then` p')
       where p' = unsafeCoerce p
             q' = unsafeCoerce q
